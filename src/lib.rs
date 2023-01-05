b6ec6005bbfa6342aa6ebbd89134347bf8ca4210
silvanshade <silvanshade@users.noreply.github.com>
2023-01-04 23:26:57 -0700

    Implement method to get all cookies for webview2

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,14 @@ use futures::{future::BoxFuture, prelude::*};
 
 pub type BoxError = Box<dyn std::error::Error + Send + Sync + 'static>;
 
-pub trait WebviewExt: private::Sealed {
-    fn webview_clear_cache(&self) -> BoxFuture<Result<(), BoxError>>;
-    fn webview_clear_cookies(&self) -> BoxFuture<Result<(), BoxError>>;
-    fn webview_navigate(&self, url: url::Url) -> Result<(), BoxError>;
-}
+#[cfg(any(
+    target_os = "linux",
+    target_os = "dragonfly",
+    target_os = "freebsd",
+    target_os = "openbsd",
+    target_os = "netbsd"
+))]
+use webkit2gtk::{WebContextExt, WebViewExt, WebsiteDataManagerExtManual, WebsiteDataTypes};
 
 #[cfg(target_os = "macos")]
 use ::{
@@ -17,6 +20,92 @@ use ::{
     },
     objc::*,
 };
+
+#[cfg(target_os = "windows")]
+use ::{
+    tauri::window::PlatformWebview,
+    webview2_com::{
+        CallDevToolsProtocolMethodCompletedHandler,
+        Error::WindowsError,
+        Microsoft::Web::WebView2::Win32::{ICoreWebView2Cookie, COREWEBVIEW2_COOKIE_SAME_SITE_KIND},
+    },
+    windows::{
+        core::{HSTRING, PCWSTR, PWSTR},
+        w,
+        Win32::Foundation::BOOL,
+    },
+};
+
+#[derive(Clone, Debug)]
+pub struct Cookie {
+    pub name: String,
+    pub value: String,
+    pub domain: String,
+    pub path: String,
+    pub expires: f64,
+    pub is_http_only: bool,
+    pub same_site: i32,
+    pub is_secure: bool,
+    pub is_session: bool,
+}
+
+#[cfg(target_os = "windows")]
+impl TryFrom<ICoreWebView2Cookie> for Cookie {
+    type Error = BoxError;
+
+    fn try_from(cookie: ICoreWebView2Cookie) -> Result<Self, Self::Error> {
+        let name = &mut PWSTR::null();
+        let value = &mut PWSTR::null();
+        let domain = &mut PWSTR::null();
+        let path = &mut PWSTR::null();
+        let expires = &mut f64::default();
+        let is_http_only = &mut BOOL::default();
+        let same_site = &mut COREWEBVIEW2_COOKIE_SAME_SITE_KIND::default();
+        let is_secure = &mut BOOL::default();
+        let is_session = &mut BOOL::default();
+
+        unsafe {
+            cookie.Name(name)?;
+            cookie.Value(value)?;
+            cookie.Domain(domain)?;
+            cookie.Path(path)?;
+            cookie.Expires(expires)?;
+            cookie.IsHttpOnly(is_http_only)?;
+            cookie.SameSite(same_site)?;
+            cookie.IsSecure(is_secure)?;
+            cookie.IsSession(is_session)?;
+
+            let name = name.to_string()?;
+            let value = value.to_string()?;
+            let domain = domain.to_string()?;
+            let path = path.to_string()?;
+            let expires = *expires;
+            let is_http_only = is_http_only.as_bool();
+            let same_site = same_site.0;
+            let is_secure = is_secure.as_bool();
+            let is_session = is_session.as_bool();
+
+            Ok(Self {
+                name,
+                value,
+                domain,
+                path,
+                expires,
+                is_http_only,
+                same_site,
+                is_secure,
+                is_session,
+            })
+        }
+    }
+}
+
+pub trait WebviewExt: private::Sealed {
+    fn webview_clear_cache(&self) -> BoxFuture<Result<(), BoxError>>;
+    fn webview_clear_cookies(&self) -> BoxFuture<Result<(), BoxError>>;
+    fn webview_get_all_cookies(&self) -> BoxFuture<Result<Option<Vec<Cookie>>, BoxError>>;
+    fn webview_navigate(&self, url: url::Url) -> Result<(), BoxError>;
+}
 
 #[cfg(target_os = "macos")]
 impl WebviewExt for tauri::Window {
@@ -99,15 +188,6 @@ impl WebviewExt for tauri::Window {
     target_os = "openbsd",
     target_os = "netbsd"
 ))]
-use webkit2gtk::{WebContextExt, WebViewExt, WebsiteDataManagerExtManual, WebsiteDataTypes};
-
-#[cfg(any(
-    target_os = "linux",
-    target_os = "dragonfly",
-    target_os = "freebsd",
-    target_os = "openbsd",
-    target_os = "netbsd"
-))]
 impl WebviewExt for tauri::Window {
     #[cfg_attr(feature = "tracing", tracing::instrument)]
     fn webview_clear_cache(&self) -> BoxFuture<Result<(), BoxError>> {
@@ -165,16 +245,6 @@ impl WebviewExt for tauri::Window {
         .map_err(Into::into)
     }
 }
-
-#[cfg(target_os = "windows")]
-use ::{
-    tauri::window::PlatformWebview,
-    webview2_com::{CallDevToolsProtocolMethodCompletedHandler, Error::WindowsError},
-    windows::{
-        core::{HSTRING, PCWSTR},
-        w,
-    },
-};
 
 #[cfg(target_os = "windows")]
 impl WebviewExt for tauri::Window {
@@ -250,6 +320,84 @@ impl WebviewExt for tauri::Window {
                 .and(call_rx.await?)?;
             done_rx.await?;
             Ok(())
+        }
+        .boxed()
+    }
+
+    #[cfg_attr(feature = "tracing", tracing::instrument)]
+    fn webview_get_all_cookies(&self) -> BoxFuture<Result<Option<Vec<Cookie>>, BoxError>> {
+        use std::sync::{Arc, Mutex};
+        use webview2_com::{
+            GetCookiesCompletedHandler,
+            Microsoft::Web::WebView2::Win32::{ICoreWebView2CookieList, ICoreWebView2_15},
+        };
+
+        #[derive(Debug)]
+        struct GetCookiesResult(Arc<Mutex<ICoreWebView2CookieList>>);
+
+        impl GetCookiesResult {
+            fn new(list: ICoreWebView2CookieList) -> Self {
+                Self(Arc::new(Mutex::new(list)))
+            }
+
+            fn inner(&self) -> &Arc<Mutex<ICoreWebView2CookieList>> {
+                &self.0
+            }
+        }
+
+        unsafe impl Send for GetCookiesResult {
+        }
+        unsafe impl Sync for GetCookiesResult {
+        }
+
+        unsafe fn run(
+            webview: PlatformWebview,
+            done_tx: tokio::sync::oneshot::Sender<Option<GetCookiesResult>>,
+        ) -> Result<(), wry::Error> {
+            let webview = webview.controller().CoreWebView2().map_err(WindowsError)?;
+            let webview = windows::core::Interface::cast::<ICoreWebView2_15>(&webview).map_err(WindowsError)?;
+            let manager = webview.CookieManager().map_err(WindowsError)?;
+            GetCookiesCompletedHandler::wait_for_async_operation(
+                Box::new(move |handler| {
+                    manager.GetCookies(w!(""), &handler)?;
+                    Ok(())
+                }),
+                Box::new(move |hresult, list| {
+                    hresult?;
+                    #[cfg(feature = "tracing")]
+                    tracing::info!(?list);
+                    done_tx.send(list.map(GetCookiesResult::new)).unwrap();
+                    Ok(())
+                }),
+            )?;
+            Ok(())
+        }
+
+        let window = self.clone();
+        async move {
+            let (done_tx, done_rx) = tokio::sync::oneshot::channel();
+            let (call_tx, call_rx) = tokio::sync::oneshot::channel();
+            window
+                .with_webview(move |webview| unsafe {
+                    let result = run(webview, done_tx).map_err(Into::into);
+                    call_tx.send(result).unwrap();
+                })
+                .map_err(Into::<BoxError>::into)
+                .and(call_rx.await?)?;
+            if let Some(list) = done_rx.await? {
+                let list = list.inner().lock().unwrap();
+                let mut cookies = Vec::<Cookie>::new();
+                unsafe {
+                    let count = &mut u32::default();
+                    list.Count(count)?;
+                    for i in 0 .. *count - 1 {
+                        cookies.push(list.GetValueAtIndex(i)?.try_into()?);
+                    }
+                }
+                Ok(Some(cookies))
+            } else {
+                Ok(None)
+            }
         }
         .boxed()
     }
